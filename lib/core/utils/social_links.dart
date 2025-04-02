603de0629b3c7f6b9131b83a7e3e9c685219656c
Hariharan <harib@furtimtechnologies.com>
2025-04-02 22:18:42 +0530

    initial commit

---
+++ lib/core/utils/social_links.dart
@@ -0,0 +1,7 @@
+abstract class SocialLinks {
+  static const String facebook = "";
+  static const String twitter = "";
+  static const String instagram = "";
+  static const String linkedin =
+      "https://www.linkedin.com/in/hari-haran-835651297/";
+}
